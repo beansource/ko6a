654ad08fe506b00cebd6841a9b3de4acbe98cc60
swand0g <alexanderjswanson@icloud.com>
2021-11-17 15:59:03 -0500

    util and nicer fancy import

--- src/pages/projects/[[...slug]].js
+++ src/pages/projects/[[...slug]].js
@@ -1,8 +1,8 @@
 import { data } from '../../_data'
 import { useRouter } from 'next/router'
-import { Project } from '@/projects/Project'
-import { Repo } from '@/projects/Repo'
-import { Explorer } from '@/projects/Explorer'
+import { Project } from '@projects/Project'
+import { Repo } from '@projects/Repo'
+import { Explorer } from '@projects/Explorer'
 
 export default function Projects() {
   const router = useRouter()
