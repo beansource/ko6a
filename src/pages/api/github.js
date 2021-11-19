ec6723f80c6c8b9465fa9fc6021555b4a3b467e5
eric <eric.carboni@optum.com>
2021-11-19 09:46:33 -0500

    inital refactor files, directories, file sizes with graphql

--- src/pages/api/github.js
+++ src/pages/api/github.js
@@ -1,20 +1,34 @@
-import { Octokit } from '@octokit/rest'
-
-const octokit = new Octokit({
-  auth: process.env.GITHUB_AUTH_TOKEN
-})
+import { graphql } from '@octokit/graphql'
 
 export default async function handler(req, res) {
-  try {
-    const {
-      data,
-    } = await octokit.repos.getContent({
-      owner: req.body.owner,
-      repo: req.body.repo,
-      path: req.body.path
-    })
-    res.status(200).json({ data })
-  } catch (error) {
-    res.status(500).json({ error })
-  }
+  const { repository } = await graphql({
+    query: `query repository($owner: String!, $repo: String!)
+    {
+      repository(owner:$owner, name:$repo) {
+        description
+        stargazerCount
+        updatedAt
+        object(expression: "HEAD:") {
+          ...on Tree {
+            entries {
+              name
+              type
+              object {
+                ... on Blob {
+                  byteSize
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `,
+  owner: req.body.owner,
+  repo: req.body.repo,
+  headers: {
+    authorization: `token ${process.env.GITHUB_AUTH_TOKEN}`
+    }
+  })
+  res.status(200).json({ repository })
 }
