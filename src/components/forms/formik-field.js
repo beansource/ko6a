89db5a1bc1b40b00ad948706717692f9f3fbd459
Alexander Swanson <alexanderjswanson@icloud.com>
2021-12-04 11:27:41 -0500

    feat: add repos 🧮 (#26)
    * add repos to project
    
    * remove console log

---
+++ src/components/forms/formik-field.js
@@ -0,0 +1,16 @@
+import { Field } from 'formik'
+import { Input, FormControl, FormLabel, FormErrorMessage} from '@chakra-ui/react'
+
+export default function FormikField({ name, label, placeholder, validation }) {
+  return (
+    <Field name={name} validate={validation}>
+      {({ field, form }) => (
+        <FormControl isInvalid={form.errors[field.name] && form.touched[field.name]}>
+          <FormLabel htmlFor={field.name}>{label}</FormLabel>
+          <Input {...field} id={field.name} placeholder={placeholder} />
+          <FormErrorMessage>{form.errors[field.name]}</FormErrorMessage>
+        </FormControl>
+      )}
+    </Field>
+  )
+}
