ca98e9e17c4d5317978a43a1866b7ad8a2203d63
swand0g <alexanderjswanson@icloud.com>
2021-12-20 14:46:18 -0500

    delete unused garbo

--- src/lib/supabase/index.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-let supabaseClient
-export default supabaseClient = createClient(process.env.NEXT_PUBLIC_SUPABASE_HOST, process.env.NEXT_PUBLIC_SUPABASE_KEY)
