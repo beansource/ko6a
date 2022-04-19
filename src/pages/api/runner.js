698fda8b99b77015ac5e77e2579851a06608ac7f
eric <eric.carboni@optum.com>
2022-04-19 16:27:06 -0400

    add json output, initial take on schema

--- src/pages/api/runner.js
+++ src/pages/api/runner.js
@@ -1,14 +1,22 @@
 import { exec, spawn } from 'child_process'
 import { Readable } from 'stream'
+const fs = require('fs-extra')
 
 export default async function handler(req, res) {
   const path = req?.body?.path
-  let options
+  const timestamp = Date.now()
+
+  try {
+    await fs.ensureFile(`results/${path}/${timestamp}.json`)
+    console.log('created dir & file for results')
+  } catch (err) {
+    console.error(err)
+  }
   
   let k6 = spawn('k6', [
     'run',
     '--out',
-    'json=results/latest.json',
+    `json=results/${path}/${timestamp}.json`,
     req?.body?.script
   ])
 
@@ -25,7 +33,7 @@ export default async function handler(req, res) {
   })
   
   k6.on('exit', code => {
-    exec('cat results/latest.json', (error, stdout, stderr) => {
+    exec(`cat results/${path}/${timestamp}.json`, (error, stdout, stderr) => {
       res.end(`child process exited with code ${code.toString()}`)
     })
   })
