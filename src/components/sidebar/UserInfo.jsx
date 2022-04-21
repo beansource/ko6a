a986d9178a226ec393f46660768eabd28f95d73c
tiff <tiffany.nguyen1@optum.com>
2022-04-21 15:47:51 -0400

    fixing sidebar overflow

--- src/components/sidebar/UserInfo.jsx
+++ src/components/sidebar/UserInfo.jsx
@@ -5,15 +5,14 @@ export const UserInfo = props => {
   const { name, image, email } = props
 
   return (
-    <HStack display="inline-flex" p="3">
+    <HStack display="inline-flex" p="3" maxW="100%">
       <Avatar bg="none" size="sm" name={name} src={image} />
-      <Box lineHeight="1" align="left">
-        <Text fontWeight="semibold">{name}</Text>
-        <Text fontSize="xs" mt="1" color={mode('whiteAlpha.700', 'gray.400')}>
+      <Box lineHeight="1" align="left" w="70%" >
+        <Text fontWeight="semibold" textOverflow="ellipsis" whiteSpace="nowrap" overflow="hidden">{name}</Text>
+        <Text fontSize="xs" mt="1" color={mode('whiteAlpha.700', 'gray.400')} textOverflow="ellipsis" whiteSpace="nowrap" overflow="hidden">
           {email}
         </Text>
       </Box>
-      <Spacer />
       <UpDownIcon />
     </HStack>
   )
