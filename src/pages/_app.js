d7a2cd2ffc494f22edb00571d8deff6135df3c24
Eric Carboni <ericcarboni@gmail.com>
2022-06-06 21:17:57 -0400

    chore(version): update to react 18

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,35 +1,31 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ChakraProvider, createLocalStorageManager } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
-import { data } from '../_data'
 import Head from 'next/head'
 import theme from '../theme'
 import Layout from '../components/layout'
 import { SWRConfig } from 'swr'
 import { $fetch } from 'ohmyfetch'
 import { QueryClient, QueryClientProvider } from 'react-query'
+const manager = createLocalStorageManager('chakra-color')
 
 function Ko6a({ Component, pageProps: { session, ...pageProps }}) {
   const queryClient = new QueryClient()
 
   return (
     <SessionProvider session={session}>
-      <ChakraProvider resetCSS theme={theme}>
-        <ColorModeProvider
-          options={{ useSystemColorMode: false }}
-        >
-          <SWRConfig value={{ fetcher: $fetch }}>
-            <QueryClientProvider client={queryClient}>
-              <Head>
-                <title>
-                  {data.title}
-                </title>
-              </Head>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </QueryClientProvider>
-          </SWRConfig>
-        </ColorModeProvider>
+      <ChakraProvider resetCSS theme={theme} colorModeManager={manager}>
+        <SWRConfig value={{ fetcher: $fetch }}>
+          <QueryClientProvider client={queryClient}>
+            <Head>
+              <title>
+                Ko6a
+              </title>
+            </Head>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </QueryClientProvider>
+        </SWRConfig>
       </ChakraProvider>
     </SessionProvider>
   )
