7f3ee41f4b2a6b8009c65941af031913cbd05ded
swand0g <alexanderjswanson@icloud.com>
2022-06-11 19:13:08 -0400

    team settings team list

--- src/pages/settings/teams/index.jsx
+++ src/pages/settings/teams/index.jsx
@@ -1,15 +1,41 @@
-import { Center } from '@chakra-ui/react'
+import { Center, Container, Heading, List, ListItem, Text, VStack, Image, HStack, Spacer, Button } from '@chakra-ui/react'
 import { useUser } from '@util/hooks'
+import Link from 'next/link'
 
 const TeamSettingsHome = () => {
-const { user } = useUser()
-console.log("🚀 ~ file: index.jsx ~ line 6 ~ TeamSettingsHome ~ user", user)
+  const { user } = useUser()
+  const teams = user?.teams.filter(team => team.name !== user.defaultTeam)
   
   return (
-    <Center h='100vh'>
-      This shall list all teams you are a member of
-    </Center>
+    <Container maxW='4xl'>
+      <Heading as="h2" fontSize='24px' mb='4'>
+        Teams
+      </Heading>
+      <List borderWidth='1px' borderRadius='6px' borderStyle='solid'>
+        {teams.map(team => (
+          <ListItem key={team.id} padding='16px'>
+            <HStack>
+              <Image
+                width='30px' height='30px' borderRadius={'50%'}
+                src={team.avatarUrl ?? `https://avatars.dicebear.com/api/jdenticon/${team.name}.svg`}
+              />
+              <Text>
+                {team.name}
+              </Text>
+              <Spacer />
+              <Link href={`/settings/teams/${team.name}`} passHref>
+                <Button size='sm' as='a'>
+                  Settings
+                </Button>
+              </Link>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </Container>
   )
 }
+
+
 
 export default TeamSettingsHome
