b3495570650e0e97aec0041a0b082023dbe3ed1c
swand0g <alexanderjswanson@icloud.com>
2022-06-12 14:59:58 -0400

    dark/light mode toggle in settings

--- src/pages/settings/appearance.jsx
+++ src/pages/settings/appearance.jsx
@@ -1,8 +1,26 @@
+import { Container, Heading, HStack, Spacer, Switch, useColorMode } from "@chakra-ui/react"
+
 const AppearanceSettings = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
-    <div>
-      <h1>Appearance Settings</h1>
-    </div>
+    <Container maxW='4xl'>
+      <Heading as='h2' fontSize='24px' mb='4'>
+        Appearance
+      </Heading>
+
+      {/* create a toggle for chakra ui dark mode */}
+      <Heading as='h3' fontSize='16px' mb='4'>
+        Dark mode
+      </Heading>
+      <HStack w='full'>
+        <Heading as='h4' fontSize='12px' mb='4'>
+          Toggle dark mode
+        </Heading>
+        <Spacer />
+        <Switch isChecked={colorMode === "dark"} onChange={toggleColorMode} />
+      </HStack>
+    </Container>
   )
 }
 
