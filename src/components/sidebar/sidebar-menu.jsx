8b8e4be2f465c8e855e17ab8c2810607794351ac
swand0g <alexanderjswanson@icloud.com>
2022-06-18 11:43:02 -0400

    use different icons & cleanup

--- src/components/sidebar/sidebar-menu.jsx
+++ src/components/sidebar/sidebar-menu.jsx
@@ -1,23 +1,25 @@
 import { useContext } from 'react'
 import { useDisclosure } from '@chakra-ui/hooks'
-import { Menu as ChakraMenu, MenuButton, MenuList, MenuItem,
-  Spinner, Image, Text, Divider, useColorMode, HStack, Heading, Spacer, Switch, Button } from '@chakra-ui/react'
-import { Moon, Sun } from 'lucide-react'
-import { SettingsIcon } from '@chakra-ui/icons'
+import { SettingsIcon, MoonIcon, SunIcon } from '@chakra-ui/icons'
 import { FiPlusCircle, FiLogOut } from 'react-icons/fi'
 import { UserInfo } from './user-info'
 import NewTeam from '@components/teams/new-team'
-
 import { TeamContext } from '@components/contexts/team-context'
 
 import { signOut } from 'next-auth/react'
 import { useUser } from '@util/hooks'
 
+import {
+  Menu as ChakraMenu, MenuButton, MenuList, MenuItem as ChakraMenuItem, Spinner, Image, Text, Divider, useColorMode,
+  useColorModeValue
+} from '@chakra-ui/react'
+
 export default function SidebarMenu({ teams, isTeamsLoading }) {
   const { user } = useUser()
   const { currentTeam, handleTeamSwitch } = useContext(TeamContext)
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const { colorMode, toggleColorMode } = useColorMode()
+
+  const menuItemSectionTextColor = useColorModeValue('gray.600', 'gray.100')
   
   return (
     <ChakraMenu>
@@ -30,9 +32,14 @@ export default function SidebarMenu({ teams, isTeamsLoading }) {
       <MenuList p="2" borderRadius="base" w="full" >
         {isTeamsLoading ? <Spinner /> : (
           <>
-            <Text p="0 8px 4px 8px" color="gray.600">Personal</Text>
+            <Text
+              p="0 8px 4px 8px" color={menuItemSectionTextColor}
+            >
+              Personal
+            </Text>
             <MenuItem
-              as="button" onClick={() => handleTeamSwitch(user.ghLogin)} color="gray.700" borderRadius="base" p="8px"
+              as="button" borderRadius="base" p="8px"
+              onClick={() => handleTeamSwitch(user.ghLogin)} 
             >
               <Image
                 boxSize='20px'
@@ -41,12 +48,20 @@ export default function SidebarMenu({ teams, isTeamsLoading }) {
                 alt={user.ghLogin}
                 mr='12px'
               />
-              <Text fontWeight={user.ghLogin === currentTeam ? 'bold' : 'normal'}>{user.ghLogin}</Text>
+              <Text fontWeight={user.ghLogin === currentTeam ? 'bold' : 'normal'}>
+                {user.ghLogin}
+              </Text>
             </MenuItem>
-            <Text p="0 8px 4px 8px" color="gray.600">Teams</Text>
+
+            <Text p="0 8px 4px 8px" color={menuItemSectionTextColor}>
+              Teams
+            </Text>
             {teams.map(teamItem => (user.ghLogin != teamItem.name ? 
-              <MenuItem as="button" color="gray.700" borderRadius="base" p="8px" key={`${teamItem.name}-menu-item`} 
-                onClick={() => handleTeamSwitch(teamItem.name)}>
+              <MenuItem
+                as="button" borderRadius="base" p="8px"
+                key={`${teamItem.name}-menu-item`}
+                onClick={() => handleTeamSwitch(teamItem.name)}
+              >
                 <Image
                   boxSize='20px'
                   borderRadius='full'
@@ -54,64 +69,34 @@ export default function SidebarMenu({ teams, isTeamsLoading }) {
                   alt={teamItem.name}
                   mr='12px'
                 />
-                <Text fontWeight={teamItem.name === currentTeam ? 'bold' : 'normal'}>{teamItem.name}</Text>
+                <Text fontWeight={teamItem.name === currentTeam ? 'bold' : 'normal'}>
+                  {teamItem.name}
+                </Text>
               </MenuItem>
             : null))}
             <MenuItem
-              p="8px" 
-              color="gray.700" 
-              borderRadius="base" 
-              onClick={onOpen} 
+              p="8px"
+              borderRadius="base"
+              onClick={onOpen}
               icon={<FiPlusCircle size="20px" color="#2780ce"/>}
             >
               Create Team
             </MenuItem>
             <Divider my="2"/>
-            <MenuItem
-              as='button'
-              p="8px"
-              color="gray.700"
-              borderRadius="base"
-              onClick={toggleColorMode}
-              icon={<Moon size="15px" /> }
-            >
-              Dark Mode
-            </MenuItem>
-            {/* {colorMode === 'light' ? (
-              <MenuButton
-                p="8px"
-                color="gray.700"
-                borderRadius="base"
-                onClick={toggleColorMode}
-                icon={<Moon size="15px" /> }
-              >
-                Dark Mode
-              </MenuButton>
-            ) : (
-              <MenuItem
-                p="8px"
-                color="gray.700"
-                borderRadius="base"
-                onClick={toggleColorMode}
-                icon={<Sun size="15px" /> }
-              >
-                Light Mode
-              </MenuItem>
-            )} */}
+
+            <ColorModeToggle />
             <MenuItem
               as="a"
-              p="8px" 
-              color="gray.700" 
-              borderRadius="base" 
-              icon={<SettingsIcon boxSize="15px" color="gray.600" />}
+              p="8px"
+              borderRadius="base"
+              icon={<SettingsIcon boxSize="15px" />}
               href={`/settings/teams/${currentTeam}`}
             >
               Team Settings
             </MenuItem>
             <MenuItem
-              p="8px" 
-              color="gray.700" 
-              borderRadius="base" 
+              p="8px"
+              borderRadius="base"
               onClick={() => signOut()}
               icon={<FiLogOut size="15px" color="#4A5568" />}
             >
@@ -120,7 +105,43 @@ export default function SidebarMenu({ teams, isTeamsLoading }) {
           </>
         )}
       </MenuList>
-      <NewTeam isOpen={isOpen} onOpen={onOpen} onClose={onClose}/>
+      <NewTeam isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
     </ChakraMenu>
+  )
+}
+
+/**
+ * Small wrapper for MenuItem's text color
+ */
+const MenuItem = ({ children, ...props }) => {
+  const menuItemTextColor = useColorModeValue('gray.700', 'gray.200')
+  return (
+    <ChakraMenuItem color={menuItemTextColor} {...props}>
+      {children}
+    </ChakraMenuItem>
+  )
+}
+
+const ColorModeToggle = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+  const itemText = colorMode === 'light' ? 'Dark Mode' : 'Light Mode'
+  const itemIcon = colorMode === 'light' ? <MoonIcon boxSize="15px" /> : <SunIcon boxSize="15px" />
+
+  // Delay in place because chakra toggles the entire UI and abruptly closes the menu
+  const delayedToggle = () => {
+    setTimeout(() => {
+      toggleColorMode()
+    }, 200)
+  }
+
+  return (
+    <MenuItem
+      p="8px"
+      borderRadius="base"
+      onClick={delayedToggle}
+      icon={itemIcon}
+    >
+      {itemText}
+    </MenuItem>
   )
 }
